2de3807157b2d44f030928910c39bc1d14988cbc
Joshua González Santana <canary_styl3@hotmail.com>
2022-01-21 20:05:40 +0100

    Create pow.bat

---
+++ BatchScripts/pow.bat
@@ -0,0 +1,11 @@
+@echo off
+set n=%1
+if "%1"==""	set /p n="Escribe el numero: "	
+set p=%2
+if "%2"=="" set /p p="Escribe la potencia: "
+
+set total=1
+for /L %%i in (1,1,%p%) do (
+	set /a total=total*%n%
+)
+echo %total%
