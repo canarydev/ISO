f01fdef680adff964ea7af8e53221b90a5cca5bc
Joshua González Santana <canary_styl3@hotmail.com>
2022-03-10 16:39:25 +0100

    Update pow.bat

--- BatchScripts/pow.bat
+++ BatchScripts/pow.bat
@@ -1,8 +1,8 @@
 @echo off
 set n=%1
-if "%1"==""	set /p n="Escribe el numero: "	
+if [%1]==[]	set /p n="Escribe el numero: "	
 set p=%2
-if "%2"=="" set /p p="Escribe la potencia: "
+if [%2]==[] set /p p="Escribe la potencia: "
 
 set total=1
 for /L %%i in (1,1,%p%) do (
